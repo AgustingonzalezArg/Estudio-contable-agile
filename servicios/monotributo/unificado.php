d5afc23eec30f4a84a3b23fea4277aa0e0af5ad3
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-07-22 12:07:51 -0300

    creacion de monotributo-unificado

---
+++ servicios/monotributo/unificado.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Realizamos la gestion de monotributo unificado para cordoba, de forma digital y en menos de 48 hs ✔ Velocidad ✔ Eficiencia ✔ Digital / online ✔ Realizado por expertos en el rubro.">
+    <link rel="stylesheet" href="../../styles/style.css">
+    <link rel="stylesheet" href="../../styles/index.css">
+    <link rel="stylesheet" href="../../styles/TSR.css">
+    <link rel="stylesheet" href="../../styles/servicios/monotributo/unificado.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Monotributo unificado - Cordoba</title>
+</head>
+<body>
+    <header>
+        <div class="container-nav">
+            <?php include '../../includes/nav.php'?>
+        </div>
+        <?php include '../../includes/logo.php'?>
+    </header>
+    <main>
+        <section class="section-1">
+            <h1>Monotributo unificado para Córdoba</h1>
+            <p>El monotributo unificado combina trámites nacionales y provinciales en un solo pago de impuestos, simplificando así la carga administrativa para los contribuyentes. Realizamos esta gestión de forma rápida y online.</p>
+            <div class="container-a">
+                <a href="">Hacer mi monotributo unificado</a>
+            </div>
+        </section>
+        
+        <section class="section-2">
+            <h2> ¿Qué es el monotributo unificado?</h2>
+            <p>El monotributo unificado simplifica la carga administrativa al agrupar trámites nacionales y provinciales en un único pago de impuestos. Se paga una cuota mensual fija y no se aplican retenciones en cuentas bancarias ni en tarjetas de débito y crédito. Además, elimina la obligación de presentar la declaración jurada mensual y anual de ingresos brutos.</p>
+            <p>Esta informacion fue obtenida de la pagina de AFIP</p>
+        </section>
+        
+        <section class="agile-service">
+            <img src="../../assets/logo/logoTransparente.webp" alt="logo de estudio contable Agile">
+            <h2>SERVICIO AGILE: Alta monotributo para deliverys y choferes</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ducimus totam perferendis nam rerum ex, modi provident, ad nemo ipsam expedita. Necessitatibus</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <ul>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+            </ul>
+            <div class="container-a">
+                <a href="">Unificar mi monotributo</a>
+            </div>
+        </section>
+
+        <section class="section-informational">
+            <h2>¿Qué incluye el pago del monotributo unificado?</h2>
+            <p> Al tener el monotributo unificado podés pagar o abonar en un solo pago todo lo siguiente:</p>
+            <ul>
+                <li>El impuesto integrado.</li>
+                <li>Aportes correspondientes al Sistema Integrado Previsional Argentino (SIPA).</li>
+                <li>Si correspondiera, también el pago al Sistema Nacional del Seguro de Salud.</li>
+                <li>Impuesto sobre los ingresos brutos.</li>
+                <li>La contribución que afecta la actividad comercial, industrial y de servicios se aplica siempre y cuando el municipio o comuna haya firmado un convenio de colaboración con la Provincia para la recaudación de dicho tributo.</li>
+            </ul>   
+        </section>
+        
+        <section class="section-informational">
+            <h2>¿Quiénes pueden tener monotributo simplificado en 2024?</h2>
+            <p>El monotributo simplificado, que es un sinónimo del monotributo unificado, está destinado a los ciudadanos que se encuentran alcanzados por el impuesto sobre los Ingresos Brutos (solo las provincias adheridas) y por la contribución municipal.</p>    
+            <p>Las provincias adheridas al monotributo unificado son: Entre Ríos, Mendoza, Córdoba, San Juan, Jujuy, Salta, Río Negro, Buenos Aires, Neuquén, Santa Cruz, Chaco, Catamarca y Tierra del Fuego.</p>
+            <div class="container-a">
+                <a href="">Hacer mi monotributo unificado</a>
+            </div>
+        </section>
+        
+        
+        <section class="cluster-other-services">
+            <h2>Otros servicios que te pueden interesar</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/recategorizacion.php">
+                        <img src="../../assets/imgs/recategorizacion-monotributo.webp" alt="Recategorizacion de monotributo">
+                        <h3>Recategorización Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/alta.php">
+                        <img src="../../assets/imgs/alta-monotributo.webp" alt="Alta del monotributo">
+                        <h3>Alta Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <img src="../../assets/imgs/creacion-sas.webp" alt="Creación de SAS - Sociedad por acciones Simplificadas">
+                        <h3>Devolución impuesto pais</h3>
+                    </a>
+                </article>
+            </div>
+        </section>
+
+        <section class="cluster-services">
+            <h2>Todos nuestros servicios</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo.php">
+                        <h3>MONOTRIBUTO</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/responsable-inscripto.php">
+                        <h3>RESPONSABLES INSCRIPTOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/liquidacion-sueldos.php">
+                        <h3>LIQUIDACIÓN DE SUELDOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/empleadas-domesticas.php">
+                        <h3>SERVICIOS EMP. DOMESTICAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/bienes-personales.php">
+                        <h3>BIENES PERSONALES</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <h3>GESTIÓN DEVOLUCIÓN IMPUESTO PAIS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/declaracion-ganacias.php">
+                        <h3>DECLARACIÓN DE GANANCIAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/convenio-multilateral.php">
+                        <h3>CONVENIO MULTILATERAL</h3>
+                    </a>
+                </article>
+            </div>
+        </section>
+    </main>
+    <?php include '../../includes/whatsappBtn.php'?>
+    <?php include '../../includes/footer.php'?>
+</body>
+</html>
