200ac81c92321cb36da5c9f14f3f36d7f0d2fbc2
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-07-22 11:24:01 -0300

    Alta PedidosYa creado

---
+++ servicios/monotributo/pedidosya.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Generamos planes de pagos y la mejor estrategia para salir de deudas en el monotributo. Somos un estudio contable digital que trabaja de forma online para todo el pais.">
+    <link rel="stylesheet" href="../../styles/style.css">
+    <link rel="stylesheet" href="../../styles/index.css">
+    <link rel="stylesheet" href="../../styles/TSR.css">
+    <link rel="stylesheet" href="../../styles/servicios/monotributo/pedidosya.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Deuda en el Monotributo - Solución </title>
+</head>
+<body>
+    <header>
+        <div class="container-nav">
+            <?php include '../../includes/nav.php'?>
+        </div>
+        <?php include '../../includes/logo.php'?>
+    </header>
+    <main>
+        <section class="section-1">
+            <h1>Monotributo para PedidosYa, Uber y Rappi</h1>
+            <p>En esta página te vamos a explicar cómo hacer el monotributo para repartidores de delivery sin tantas vueltas ni relleno.</p>
+            <p>El monotributo es la forma simplificada con la que podrás cumplir tus obligaciones ante la Administración Federal de Ingresos Públicos (AFIP), haciendo un único pago mensual y fijo. Pagando este fijo mensual, tenés acceso a una obra social y estás aportando a tu jubilación.</p>
+            <div class="container-a">
+                <a href="">Hacer mi monotributo ahora</a>
+            </div>
+        </section>
+        
+        <section class="section-2">
+            <h2>¿Cómo dar de alta monotributo para PedidosYa, Uber y Rappi?</h2>
+            <p>Cómo explicamos más a detalle en nuestra página general sobre el monotributo, la forma para darse de alta es la siguiente:</p>
+            <ol>
+                <li><strong>Obtener Clave Fiscal:</strong>
+                    <p>Accede a la web de AFIP y regístrate para obtener tu Clave Fiscal.</p>
+                </li>
+                <li><strong>Conseguir tu CUIT:</strong>
+                    <p>Completa el formulario de inscripción y obtén tu Clave Única de Identificación Tributaria (CUIT).</p>
+                </li>
+                <li><strong>Registrar datos biométricos:</strong>
+                    <p>Acude a una oficina de AFIP para registrar tus datos biométricos (huellas dactilares y foto).</p>
+                </li>
+                <li><strong>Darse de alta como monotributista:</strong>
+                    <p>Inicia sesión en la web de AFIP con tu Clave Fiscal. Selecciona la opción "Monotributo" y sigue los pasos para completar tu inscripción.</p>
+                </li>
+                <li><strong>Registrar datos de contacto:</strong>
+                    <p>Proporciona una dirección de correo electrónico y un número de teléfono vigente.</p>
+                </li>
+            </ol>
+            <p>Si quieres saber más sobre monotributo, sus beneficios, escalas, valores y más, <a href="http://localhost/estudio_contable_agile/servicios/monotributo.php">ingresa aquí</a>.</p>
+        </section>
+
+        <section class="agile-service">
+            <img src="../../assets/logo/logoTransparente.webp" alt="logo de estudio contable Agile">
+            <h2>SERVICIO AGILE: Alta monotributo para deliverys y choferes</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ducimus totam perferendis nam rerum ex, modi provident, ad nemo ipsam expedita. Necessitatibus</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti expedita molestias repellendus quisquam pariatur sed! Quaerat accusantium incidunt, illum fugiat laboriosam modi fuga, corrupti, inventore fugit voluptatum quia quis vel!</p>
+            <ul>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, animi deleniti obcaeca</li>
+            </ul>
+            <div class="container-a">
+                <a href="">Hacer monotributo</a>
+            </div>
+        </section>
+        
+        <section class="section-informational">
+            <h2>¿Qué necesito para dar el alta?</h2>
+            <p>Para darte de alta como monotributista, debes contar con tu clave fiscal, obtener la Clave Única de Identificación Tributaria (CUIT), registrar tus datos biométricos y proporcionar una dirección de correo electrónico y un número de teléfono vigente.</p>
+            <p>Con esta información podrás hacer el registro y facturar tus repartos o traslados.</p>
+        </section>
+        
+        <section class="section-informational">
+            <h2>¿Beneficios del monotributo para repartidores y conductores?</h2>
+            <p>Algunos de los beneficios con los que contás a la hora de ser monotributista son los siguientes:</p>
+            <ul>
+                <li>Dinero Blanqueado</li>
+                <li>Acceso a créditos y bancos</li>
+                <li>Obra social y los aportes jubilatorios</li>
+                <li>Menos obligaciones administrativas</li>
+            </ul>
+            <p>Informate mas sobre este tema <a href="http://localhost/estudio_contable_agile/servicios/monotributo/alta.php">aquí</a></p>
+        </section>
+        
+        
+        <section class="cluster-other-services">
+            <h2>Otros servicios que te pueden interesar</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/recategorizacion.php">
+                        <img src="../../assets/imgs/recategorizacion-monotributo.webp" alt="Recategorizacion de monotributo">
+                        <h3>Recategorización Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/gestion.php">
+                        <img src="../../assets/imgs/gestion-monotributo.webp" alt="Gestión del monotributo mensual">
+                        <h3>Gestión del Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <img src="../../assets/imgs/creacion-sas.webp" alt="Creación de SAS - Sociedad por acciones Simplificadas">
+                        <h3>Devolución impuesto pais</h3>
+                    </a>
+                </article>
+            </div>
+        </section>
+
+        <section class="cluster-services">
+            <h2>Todos nuestros servicios</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo.php">
+                        <h3>MONOTRIBUTO</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/responsable-inscripto.php">
+                        <h3>RESPONSABLES INSCRIPTOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/liquidacion-sueldos.php">
+                        <h3>LIQUIDACIÓN DE SUELDOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/empleadas-domesticas.php">
+                        <h3>SERVICIOS EMP. DOMESTICAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/bienes-personales.php">
+                        <h3>BIENES PERSONALES</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <h3>GESTIÓN DEVOLUCIÓN IMPUESTO PAIS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/declaracion-ganacias.php">
+                        <h3>DECLARACIÓN DE GANANCIAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/convenio-multilateral.php">
+                        <h3>CONVENIO MULTILATERAL</h3>
+                    </a>
+                </article>
+            </div>
+        </section>
+    </main>
+    <?php include '../../includes/whatsappBtn.php'?>
+    <?php include '../../includes/footer.php'?>
+</body>
+</html>
