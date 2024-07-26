a88061d77cdfe491c0795355afa363a2707c4eff
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-07-26 08:45:38 -0300

    creacion de empleada domestica

--- servicios/responsable-inscripto/alta.php
+++ servicios/responsable-inscripto/alta.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Baja del monotributo, como hacer el proceso para no pagar más y acumular deuda con AFIP. Toda la informacion aquí.">
+    <meta name="description" content="El paso a paso y toda la informacion que necesitas saber para darte de alta como responsable inscripto.">
     <link rel="stylesheet" href="../../styles/style.css">
     <link rel="stylesheet" href="../../styles/index.css">
     <link rel="stylesheet" href="../../styles/TSR.css">
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <title>Baja de monotributo | EstudioAgile.com</title>
+    <title>Darse de alta como responsable inscripto en 2024</title>
 </head>
 <body>
     <header>
@@ -22,34 +22,31 @@
     </header>
     <main>
         <section class="section-1">
-            <h1>Baja de monotributo: Ayuda y asesoramiento</h1>
+            <h1>Darse de alta como responsable inscripto en 2024</h1>
             <p>La baja del monotributo se puede realizar en cualquier momento y esto puede deberse a distintas opciones como cesar tus actividades económicas, por renuncia, o porque te vas a pasar a responsable inscripto. Cualquiera sea el motivo podés realizar la baja para evitar generar una deuda con la AFIP. Para que no suceda esto el mes que solicitaste la baja deberías abonarlo y el mes que viene ya se haría efectiva la baja.</p>
             <div class="container-a">
-                <a href="">Dar de baja ahora</a>
+                <a href="">Darme de alta ahora</a>
             </div>
         </section>
         
         <section class="section-2">
-            <h2>¿Me puedo dar de baja el monotributo de forma online?</h2>
-            <p>Si, el proceso para darte de baja online se realiza desde el portal de monotributo. Deberías utilizar la opción “Modificación y baja” y luego hacer clic en “Dar de baja”. A partir de allí deberías seguir los pasos que te indican</p>
-            <p>Para realizar el proceso de baja deberás contar con CUIT y una clave fiscal en AFIP.</p>
+            <h2>Como pasar de monotributo a responsable inscripto</h2>
+            <p>El alta como responsable inscripto (autonomo) es un proceso que se lleva a cabo para trabajadores independientes que por algun motivo u otro el regimen de monotributo no les conviene. Esto porque el regimen de monotributo cuenta con varias limitantes como límite de facturacion, limite en espacio utilizado, o menor tipos de facturas que en el regimen de responsable inscripto.</p>
+            <p>Es importante revisar cada uno de las limitantes del monotributo ya que si te excedes de los límites y no relizas el cambio a responsable inscripto, la AFIP lo hará automaticamente y esto genera que si quieres volver al monotributo tendras quie esperar 3 años</p>
+            <p>Para pasar de monotributo a responsable inscripto primero deberias de darte de baja en el monotributo y luego dar de alta el responsable inscripto.</p>
         </section>
 
 
 
         
         <section class="section-informational">
-            <h2> Como dar de baja el monotributo paso a paso</h2>
-            <p>Esta es una pequeña guía donde indicamos cómo dar de baja el monotributo de forma online. </p>
+            <h2>Paso a paso para dar de alta un responsable inscripto</h2>
+            <p>Esta es una guia muy sencilla para que sepas como realizar el paso a paso:</p>
             <ol>
-                <li>Accede al sitio web de AFIP con tu clave fiscal e ingresa tu número de CUIT y clave fiscal.</li>
-                <li>El sistema mostrará una lista de los servicios de AFIP que tienes habilitados.</li>
-                <li> Selecciona "Monotributo". Si no tienes este servicio habilitado, consulta en AFIP cómo utilizar la clave fiscal y cómo incorporar nuevos servicios.</li>
-                <li>Una vez en "Monotributo", elige la opción "Modificación y baja" en el menú de la izquierda.</li>
-                <li>El sistema mostrará varias tarjetas. Busca la que dice "Darse de baja del monotributo" y presiona el botón "Dar de baja".</li>
-                <li>Ingresa el período de la baja, indicando el mes y año. Luego selecciona el motivo de la baja y presiona el botón "Continuar".</li>
-                <li>El sistema te redirigirá al Registro Único Tributario para confirmar la operación.</li>
-                <li>Verifica los datos y listo.</li>
+                <li>Accede al sitio de AFIP con tu CUIT y Clave Fiscal para despues elegír el servicio "Sistema Registral"</li>
+                <li>En la pantalla, elige la opción "Registro Tributario".</li>
+                <li>Selecciona "Alta de impuestos". El sistema mostrará un listado donde debes seleccionar "IVA" y "Ganancias Persona Física", así como la fecha de inicio. Una vez elegido, haz clic en "Aceptar".</li>
+                <li>Realizando este proceso ya estarias dado de alta en el monotributo.</li>
             </ol>
         </section>
         
@@ -71,8 +68,24 @@
         </section>
 
         <section class="section-informational">
-            <h2>¿Qué pasa si me dieron de baja el monotributo de forma automática?</h2>
-            <p>Si te dieron la baja automática de tu monotributo significa que has acumulado una deuda con AFIP al no pagar tu monotributo durante 10 meses. Sea por el motivo que sea tu baja se efectuará automáticamente y si quieres volver a inscribirte deberás regularizar tu situación con la AFIP.</p>
+            <h2>Ventajas y Beneficios de ser responsable inscripto</h2>
+            <h3>Beneficios de ser responsable inscripto</h3>
+            <p>Para los nuevos responsables inscriptos hay tres beneficios muy buenos que deberian aprovechar al maximo:</p>
+            <ul>
+                <li>A partir de 2025, podrás beneficiarte de una alícuota un 50% más baja.</li>
+                <li>Todas las compras con Factura A que hayas realizado en los últimos 12 meses las vas a poder tomar. Esto te permitirá reducir el monto del IVA (mensual) y del Impuesto a las Ganancias (anual).</li>
+                <li>Paga el IVA a 90 días sin intereses.</li>
+            </ul>
+            <h3>Ventajas de ser responsable inscripto</h3>
+            <p>Los responsables inscripto cuantan con alguna ventajas que los monotributistas no, aquí hay algunas de estas:</p>
+            <ul>
+                <li><strong>Reduccion de IVA</strong>: Los responsables inscriptos pueden descontar el IVA de sus compras, lo que permite reducir el monto a pagar por este impuesto en sus ventas.</li>
+                <li><strong>Formalidad y Credibilidad</strong>: Al estar inscripto, se transmite una imagen de formalidad y profesionalismo, lo que puede aumentar la confianza de los clientes y proveedores.</li>
+                <li><strong>Acceso a Licitaciones y Contratos Grandes</strong>: Muchas empresas y organismos públicos prefieren o exigen trabajar con responsables inscriptos, ya que aseguran un nivel de formalidad y cumplimiento fiscal.</li>
+                <li><strong>Posibilidad de Emitir Facturas de Crédito</strong>: Pueden emitir facturas que, en ciertos casos, les permiten financiarse a través de los clientes al transformar esas facturas en créditos negociables.</li>
+                <li><strong>Deducción de Gastos</strong>: Los responsables inscriptos pueden deducir gastos necesarios para el desarrollo de su actividad, lo que puede reducir la carga tributaria.</li>
+                <li><strong>Mayor Facilidad para Exportar</strong>: Para realizar exportaciones, estar inscripto es casi siempre un requisito, lo que abre oportunidades de expansión a mercados internacionales.</li>
+            </ul>
         </section>
         
         <section class="cluster-other-services">
