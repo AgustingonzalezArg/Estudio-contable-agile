40b7f3b039e8b45410770483228bd19682801784
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-08-02 10:36:14 -0300

    Iva finalizado

---
+++ servicios/iva.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Si necesitas saber todo lo relacionado con el IVA, su liquidación, sus declaraciones juradas y gestiones mensuales este es el sitio correcto">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/TSR.css">
+    <link rel="stylesheet" href="../styles/servicios/iva.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <title>IVA Guia 2024 - Impuesto al valor agregado</title>
+</head>
+<body>
+    <header>
+        <div class="container-nav">
+            <?php include '../includes/nav.php'?>
+        </div>
+        <?php include '../includes/logo.php'?>
+    </header>
+    <main>
+        <section class="section-1">
+            <h1>IVA: Guia Completa 2024</h1>
+            <p>Si tienes dudas sobre tu IVA cuanto debes pagar y que deberias hacer para evitar tener problemas con afip podes contactarnos haciendo clíc en el siguiente botón</p>
+            <div class="container-a">
+                <a href="">Contactarnos</a>
+            </div>
+        </section>
+        
+        <section class="indice">
+            <h2>Índice de Contenidos</h2>
+            <ul>
+                <li><a href="#iva-que-es">IVA: Qué es?</a>
+                    <ul>
+                        <li><a href="#definicion-iva">Impuesto al Valor Agregado: Definición</a></li>
+                        <li><a href="#para-que-sirve-iva">¿Para qué sirve el IVA?</a></li>
+                    </ul>
+                </li>
+                <li><a href="#aspectos-generales-iva">Aspectos Generales del IVA</a>
+                    <ul>
+                        <li><a href="#vs-impuesto-valor-anadido">¿Impuesto de Valor Agregado vs. Impuesto Valor Añadido?</a></li>
+                        <li><a href="#diferentes-contextos-iva">El IVA en diferentes contextos</a>
+                            <ul>
+                                <li><a href="#iva-alimentos">IVA en alimentos</a></li>
+                                <li><a href="#iva-servicios-productos">IVA en servicios y productos</a></li>
+                                <li><a href="#iva-electricos-telecomunicaciones">IVA en servicios eléctricos y telecomunicaciones</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#exento-iva">Exento de IVA: Qué significa y para qué sirve</a></li>
+                    </ul>
+                </li>
+                <li><a href="#regulaciones-normativas-iva">Regulaciones y Normativas del IVA</a>
+                    <ul>
+                        <li><a href="#iva-rg-4240">IVA RG 4240</a></li>
+                        <li><a href="#iva-rg-3337">IVA RG 3337</a></li>
+                        <li><a href="#iva-rg-2408">IVA RG 2408</a></li>
+                        <li><a href="#iva-rg-2300">IVA RG 2300 2007</a></li>
+                    </ul>
+                </li>
+                <li><a href="#declaraciones-juradas-iva">Declaraciones Juradas de IVA</a>
+                    <ul>
+                        <li><a href="#declaracion-iva-linea">Declaración del IVA en línea</a></li>
+                        <li><a href="#declaracion-complementaria-iva">¿Qué es una declaración complementaria de IVA?</a></li>
+                    </ul>
+                </li>
+                <li><a href="#liquidacion-calculo-iva">Liquidación y Cálculo del IVA</a>
+                    <ul>
+                        <li><a href="#liquidacion-iva">¿Qué es una liquidación de IVA y cómo calcular cuánto hay que pagar?</a></li>
+                        <li><a href="#debito-fiscal-iva">IVA débito fiscal: ¿Qué es?</a></li>
+                        <li><a href="#credito-fiscal-iva">IVA crédito fiscal: ¿Qué es?</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+
+        <section class="section-informational">
+            <article id="iva-que-es">
+                <h2>IVA: ¿Qué es?</h2>
+                <br>
+                <h3 id="definicion-iva">Impuesto al Valor Agregado: Definición</h3>
+                <p>El IVA (Impuesto al Valor Agregado) es una carga impositiva que recae sobre el consumo, financiada por el consumidor final a través del precio de los productos y servicios que adquiere. Este impuesto es recaudado por los comerciantes y responsables inscriptos, quienes deben discriminarlo en sus ventas y luego declararlo ante la AFIP para cumplir con sus obligaciones tributarias.</p>
+                <h3 id="para-que-sirve-iva">¿Para qué sirve el IVA?</h3>
+                <p>El impuesto de valor agregado es, en resumen, un mecanismo del gobierno para recaudar impuestos y dinero a través del consumo de las personas. Algunos lo ven bien otros no tanto, sobre todo en cosas tan esenciales como alimentos que aunque suelen tener una tasa más baja, dicen que deberían no tener directamente.</p>
+            </article>
+            <article id="aspectos-generales-iva">
+                <h2>Aspectos Generales del IVA</h2>
+                <br>
+                <h3 id="vs-impuesto-valor-anadido">¿Impuesto de Valor Agregado vs. Impuesto Valor Añadido?</h3>
+                <p>Una confusión a aclarar es que muchas personas piensan que el Impuesto de Valor Agregado y el Impuesto Valor Añadido son distintas cosas cuando no es así. Estas dos palabras son sinónimos y se utilizan para el mismo contexto</p>
+                <h3 id="diferentes-contextos-iva">El IVA en diferentes contextos</h3>
+                <p>El iva y su precio o porcentaje va a variar dependiendo diferentes contextos porque no todos los productos tienen la misma tasa de impuesto al valor añadido. Existen tres tipos de alícuotas: 10,5%, 21% y 27%.</p>
+                <h4 id="iva-alimentos">IVA en alimentos</h4>
+                <p>En alimentos tenemos dos categorías para agrupar. El grupo que sería el grupo de comida más básico tiene un IVA del 10.5%, con una carga final tributaria del casi 36,2%. Ahora el segundo cuenta con un IVA mayor llegando al 21% que serían comidas como arroz, pan, fideos, aceite y cuentan con una carga tributaria final cerca del 41,8%</p>
+                <h4 id="iva-servicios-productos">IVA en servicios y productos</h4>
+                <p>Esta es la categoría más amplia y abarca la mayoría de bienes y comercios. En estos casos la carga del IVA se encuentra en el 21%.</p>
+                <h4 id="iva-electricos-telecomunicaciones">IVA en servicios eléctricos y telecomunicaciones</h4>
+                <p>En este caso la carga del impuesto al valor agregado es del 27% porque consideran que las personas que contratan estos servicios tienen “Más recursos”.</p>
+                <h3 id="exento-iva">Exento de IVA: ¿Qué significa y para qué sirve?</h3>
+                <p>Que un producto esté exento de IVA significa que en situación normal este producto debería tener una carga tributaria de IVA  pero por disposición del Gobierno ese producto no tendrá la carga tributaria, para de esta forma alivianar la carga impositiva y por ende reducir el precio del mismo.</p>
+                <div class="container-a">
+                    <a href="">Gestionar mi IVA correctamente</a>
+                </div>
+            </article>
+            <article id="regulaciones-normativas-iva">
+                <h2>Regulaciones y Normativas del IVA</h2>
+                <p>El Impuesto al valor añadido tiene varias regulaciones y normativas que deberías conocer. Si quieres más información puedes visitar las páginas de AFIP haciendo clic en cada una de ellas.</p>
+                <h3 id="iva-rg-4240">IVA RG 4240</h3>
+                <p>La Resolución General N° 4240/2018 dice que, si compras servicios digitales a empresas del exterior y no eres un responsable inscripto en IVA, debes seguir ciertas reglas. Si pagas a través de una empresa en tu país que maneja estos pagos, esa empresa se encargará de cobrar y liquidar el impuesto. Si pagas directamente a la empresa del exterior y no usas una empresa local para hacerlo, tú mismo tendrás que pagar el impuesto hasta el último día del mes en que hiciste el pago, usando un formulario electrónico.</p>
+                <h3 id="iva-rg-3337">IVA RG 3337</h3>
+                <p>La RG 3337 busca mejorar el control y la transparencia en las transacciones internacionales, asegurando que los contribuyentes cumplan con sus obligaciones fiscales y reporten adecuadamente las operaciones que puedan tener impacto en la recaudación del IVA. La resolución establece requisitos específicos de información y documentación para las operaciones que involucren a entidades o personas fuera del país, con el fin de prevenir la evasión fiscal y garantizar el correcto cumplimiento de las normativas vigentes.</p>
+                <h3 id="iva-rg-2408">IVA RG 2408</h3>
+                <p>La Resolución General N° 2408/2008 establece que cuando compras bienes o servicios del exterior y no eres un responsable inscripto en IVA, tienes que seguir ciertos pasos. Si pagas a través de una empresa en tu país que gestiona estos pagos, esa empresa se encargará de cobrar y pagar el IVA. Sin embargo, si pagas directamente a la empresa del exterior sin intermediarios locales, tendrás que abonar el IVA tú mismo y hacerlo hasta el último día del mes en que hiciste el pago, usando un formulario electrónico. Aunque parezca igual que la  RG 4240, esta es más reciente y detalla cómo deben actuar los intermediarios en pagos a proveedores del exterior, y qué hacer si no hay intermediarios.</p>
+                <h3 id="iva-rg-2300">IVA RG 2300/2007</h3>
+                <p>La Resolución General N° 2300/2007 establece que, si eres un comprador en Argentina y adquieres bienes o servicios del exterior, debes tener en cuenta cómo se maneja el IVA. Si compras directamente a proveedores extranjeros sin usar intermediarios locales, eres responsable de pagar el IVA correspondiente tú mismo. Debes hacerlo mediante un formulario electrónico, que debes presentar antes del final del mes en que realizaste la compra. Esta normativa asegura que el IVA se pague correctamente en estas transacciones internacionales.</p>
+            </article>
+            <article id="declaraciones-juradas-iva">
+                <h2>Declaraciones Juradas de IVA</h2>
+                <p>Las declaraciones juradas de IVA son un documento que deben presentar los responsables inscriptos ante la AFIP para mediante su página web determinar el pago de IVA. </p>
+                <h3 id="declaracion-iva-linea">Declaración del IVA en línea</h3>
+                <p>Las declaraciones de IVA se realizan de forma mensual y en línea a través de la página de AFIP generando los libros de IVA digital para luego verificar cuánto debería pagar la empresa. Este es un proceso que suelen llevar contadores o especialistas en el tema, ya que requiere cierto grado de conocimiento y expertis. Pero si te interesa gestionarlo por tu cuenta <a href="https://serviciosweb.afip.gob.ar/genericos/guiasPasoPaso/VerGuia.aspx?id=98">aquí </a>tienes una página de AFIP que indica el paso a paso</p>
+                <h3 id="declaracion-complementaria-iva">¿Qué es una declaración complementaria de IVA?</h3>
+                <p>Una declaración complementaria es una forma de rectificar o corregir errores en una declaración de IVA realizada y presentada previamente. Esto puede suceder por algún error de cálculo a la hora de realizarla o por omisión de información correspondiente.</p>
+            </article>
+            <article id="liquidacion-calculo-iva">
+                <h2>Liquidación y Cálculo del IVA</h2> 
+                <br>
+                <h3 id="liquidacion-iva">¿Qué es una liquidación de IVA y como calcular cuanto hay que pagar?</h3>
+                <p>Una liquidación de IVA es el cálculo que se realiza para determinar cuánto hay que pagar del Impuesto al valor agregado. Este es un proceso sencillo y aquí esta la formula: </p>
+                <p>Debito Fiscal - Crédito Fiscal = IVA total a pagar</p>
+                <p>Hay muy pocos casos en donde el resultado queda a favor del contribuyente. Esto puede suceder cuando uno recién está empezando y tiene que comprar stock</p>
+                <h3 id="debito-fiscal-iva">IVA débito fiscal: ¿Qué es?</h3>  
+                <p>El débito fiscal es el IVA recaudado por una empresa mediante la venta de sus productos o servicios y es lo que se le debería pagar a la AFIP</p>
+                <h3 id="credito-fiscal-iva">IVA crédito fiscal: ¿Qué es?</h3>
+                <p>El crédito fiscal es el IVA a favor que tiene una empresa mediante sus compras realizadas a sus proveedores y esto es le debería quedar a favor en AFIP para la empresa.</p>
+            </article>
+            <article>
+                <p>Si necesitas ayuda con tu IVA contactanos y te respondemos al instante</p>
+                <div class="container-a">
+                    <a href="">Contactarnos</a>
+                </div>
+            </article>
+        </section>
+
+
+
+        <section class="cluster-other-services">
+            <h2>Otros servicios que te pueden interesar</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/recategorizacion.php">
+                        <img src="../assets/imgs/recategorizacion-monotributo.webp" alt="Recategorizacion de monotributo">
+                        <h3>Recategorización Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo/gestion.php">
+                        <img src="../assets/imgs/gestion-monotributo.webp" alt="Gestión del monotributo mensual">
+                        <h3>Gestión del Monotributo</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <img src="../assets/imgs/creacion-sas.webp" alt="Creación de SAS - Sociedad por acciones Simplificadas">
+                        <h3>Devolución impuesto pais</h3>
+                    </a>
+                </article>
+            </div>
+        </section>
+
+        <section class="cluster-services">
+            <h2>Todos nuestros servicios</h2>
+            <div>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/monotributo.php">
+                        <h3>MONOTRIBUTO</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/responsable-inscripto.php">
+                        <h3>RESPONSABLES INSCRIPTOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/liquidacion-sueldos.php">
+                        <h3>LIQUIDACIÓN DE SUELDOS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/empleadas-domesticas.php">
+                        <h3>SERVICIOS EMP. DOMESTICAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/bienes-personales.php">
+                        <h3>BIENES PERSONALES</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/devolucion-impuesto-pais.php">
+                        <h3>GESTIÓN DEVOLUCIÓN IMPUESTO PAIS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/declaracion-ganacias.php">
+                        <h3>DECLARACIÓN DE GANANCIAS</h3>
+                    </a>
+                </article>
+                <article>
+                    <a href="http://localhost/estudio_contable_agile/servicios/convenio-multilateral.php">
+                        <h3>CONVENIO MULTILATERAL</h3>
+                    </a>
+                </article>
+            </div>
+        </section>s
+    </main>
+    <?php include '../includes/whatsappBtn.php'?>
+    <?php include '../includes/footer.php'?>
+</body>
+</html>
