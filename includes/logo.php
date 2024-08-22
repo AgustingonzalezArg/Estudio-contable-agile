5fa78bff2d5f72be93dee49159240e40cf95704f
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-08-22 09:26:03 -0300

    actualizacion enlaces de styles de includes

--- includes/logo.php
+++ includes/logo.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost\estudio_contable_agile\styles\logo.css">
+    <link rel="stylesheet" href="https://estudioagile.com/public_html/styles\logo.css">
     <title>Logo</title>
 </head>
 <body>
-        <a href="/" class="logo"><img src="http://localhost/estudio_contable_agile/assets/logo/logoTransparente.webp" alt="Logo de Estudio Contable Agile"></a>
+        <a href="/" class="logo"><img src="https://estudioagile.com/public_html/assets/logo/logoTransparente.webp" alt="Logo de Estudio Contable Agile"></a>
 </body>
 </html>
--- includes/nav.php
+++ includes/nav.php
@@ -4,18 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost\estudio_contable_agile\styles\nav.css">
+    <link rel="stylesheet" href="https://estudioagile.com/public_html/styles\nav.css">
     <title>Navegacion Principal</title>
 </head>
 <body>
     <nav aria-label="Navegacion Principal del sitio" class="nav-header close">
         <ul class="nav-header__ul">
-            <li><a href="http://localhost\estudio_contable_agile\index.php" title="Ir a la pagina de Inicio">Inicio</a></li>
-            <li><a href="http://localhost\estudio_contable_agile\servicios.php" title="Nuestros serivicos contables e impositivos">Servicios</a></li>
-            <li><a href="http://localhost\estudio_contable_agile\servicios\monotributo.php" title="Información sobre monotributo y nuestros serivicios especializados para ellos">Monotributo</a></li>
-            <li><a href="http://localhost\estudio_contable_agile\servicios\responsable-inscripto.php" title="Información sobre responsables Inscriptos y nuestros serivicios especializados para ellos">Responsables Inscriptos</a></li>
-            <li><a href="http://localhost\estudio_contable_agile\nosotros.php" title="Pagina que habla sobre nuestro estudio contable">Nosotros</a></li>
-            <li><a href="http://localhost\estudio_contable_agile\contacto.php" title="Pagina de contacto">Contacto</a></li>
+            <li><a href="https://estudioagile.com/public_html/index.php" title="Ir a la pagina de Inicio">Inicio</a></li>
+            <li><a href="https://estudioagile.com/public_html/servicios.php" title="Nuestros serivicos contables e impositivos">Servicios</a></li>
+            <li><a href="https://estudioagile.com/public_html/servicios\monotributo.php" title="Información sobre monotributo y nuestros serivicios especializados para ellos">Monotributo</a></li>
+            <li><a href="https://estudioagile.com/public_html/servicios\responsable-inscripto.php" title="Información sobre responsables Inscriptos y nuestros serivicios especializados para ellos">Responsables Inscriptos</a></li>
+            <li><a href="https://estudioagile.com/public_html/nosotros.php" title="Pagina que habla sobre nuestro estudio contable">Nosotros</a></li>
+            <li><a href="https://estudioagile.com/public_html/contacto.php" title="Pagina de contacto">Contacto</a></li>
 
         </ul>
         <button class="btn-open">☰</button>
