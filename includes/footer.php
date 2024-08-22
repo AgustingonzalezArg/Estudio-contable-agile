1182b8a1cfd64d6d7fd3318947d61230ec3bef77
AgustingonzalezArg <agustingonzzalez019@gmail.com>
2024-08-22 09:49:25 -0300

    creacion de config entorno global

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,12 @@
+<?php
+include 'config.php';
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://estudioagile.com/styles/footer.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>styles/footer.css">
     <title>Footer</title>
 </head>
 <body>
@@ -11,9 +14,9 @@
         <p>&copy; 2024 Estudio Contable Agile. Todos los derechos reservados.</p>
         <nav class="nav-footer" aria-label="Enlaces de pie de página">
             <ul>
-                <li><a href="http://localhost/estudio_contable_agile/politica-de-privacidad.php" title="Leer nuestra política de privacidad">Política de Privacidad</a></li>
-                <li><a href="http://localhost/estudio_contable_agile/terminos-y-condiciones.php" title="Leer nuestros términos y condiciones">Términos y Condiciones</a></li>
-                <li><a href="http://localhost/estudio_contable_agile/contacto.php" title="Contáctenos">Contacto</a></li>
+                <li><a href="<?php echo $base_url; ?>politica-de-privacidad.php" title="Leer nuestra política de privacidad">Política de Privacidad</a></li>
+                <li><a href="<?php echo $base_url; ?>terminos-y-condiciones.php" title="Leer nuestros términos y condiciones">Términos y Condiciones</a></li>
+                <li><a href="<?php echo $base_url; ?>contacto.php" title="Contáctenos">Contacto</a></li>
             </ul>
         </nav>
     </footer>
